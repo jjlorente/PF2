27ac46507d9ff69b58248b6a12684b1c8a9eae7e
JJLorente <joel.jimenez.lorente@gmail.com>
2023-07-11 22:46:47 +0200

    Skills imp json

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -60,46 +60,12 @@ export const Skills = () => {
       <div className='Skills' id="skills">
         <h1 className='title-about'><i className="fas fa-laptop-code" style={{color:"#20CA73"}}></i> Skills</h1>
         <div className='container-skills'>
-          <div className="skill">
-            <img className="img-logo angular" src={`/assets/angular.png`} alt='angular logo'></img>
-            <h3>Angular</h3>
-          </div>
-          <div className="skill">
-            <img className="img-logo react" src={`/assets/react.png`} alt='react logo'></img>
-            <h3>React</h3>
-          </div>
-          <div className="skill">
-            <img className="img-logo node" src={`/assets/node.png`} alt='node logo'></img>
-            <h3>NodeJS</h3>
-          </div>
-          <div className="skill">
-            <img className="img-logo exp" src={`/assets/express.svg`} alt='express logo'></img>
-            <h3>ExpressJS</h3>
-          </div>
-          <div className="skill">
-            <img className="img-logo" src={`/assets/tsicon.png`} alt='ts logo'></img>
-            <h3>TypeScript</h3>
-          </div>
-          <div className="skill">
-            <img className="img-logo" src={`/assets/js-logo.png`} alt='js logo'></img>
-            <h3>JavaScript</h3>
-          </div>
-          <div className="skill">
-            <img className="img-logo" src={`/assets/python.webp`} alt='python logo'></img>
-            <h3>Python</h3>
-          </div>
-          <div className="skill">
-            <img className="img-logo" src={`/assets/mongodb.png`} alt='mongodb logo'></img>
-            <h3>MongoDB</h3>
-          </div>
-          <div className="skill">
-            <img className="img-logo boot" src={`/assets/bootstrap.png`} alt='Bootstrap logo'></img>
-            <h3>Bootstrap</h3>
-          </div>
-          <div className="skill">
-            <img className="img-logo" src={`/assets/css.png`} alt='css logo'></img>
-            <h3>CSS</h3>
-          </div>
+          {skillsJSON.map((skill) => (
+            <div className="skill" key={skill.name}>
+              <img className={`img-logo ${skill.className}`} src={`/assets/${skill.imgSrc}`} alt={`${skill.name} logo`} />
+              <h3>{skill.name}</h3>
+            </div>
+          ))}
         </div>
       </div>
       <svg viewBox="0 0 1440 320" preserveAspectRatio="none" className="absolute svgSkills -top-20 sm:-top-40 lg:-top-60 left-0 w-screen h-42 md:h-60 lg:h-80">
